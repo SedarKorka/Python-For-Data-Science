dc10e1fac92f00e2430791712503961ca2bee344
Korka <lelevekorka@gmail.com>
2022-11-28 19:24:16 +0000

    Introduction

--- introduction.py
+++ introduction.py
@@ -52,3 +52,41 @@ var1 = 5
 var2 = 15
 if(var1 is var2):
     print(var1 is var2)
+
+##Membership relation
+#x in y
+
+##Control flow
+a = 30
+b = 70
+if(a > b):
+    pass
+    print("False")
+elif a<b:
+    
+    print("b is greater")
+else:
+    print("True")
+    pass
+
+##Data structure
+#Tupe, set , list and dictionary
+
+#List and their common operation
+square = [1,2,5,4,5,7,8]
+print(square[-2])
+for ax in square:
+    print (ax)
+
+##Example of the list of the method
+square.sort()
+print(square)
+
+##Tuple
+Tuple_List = "A",2,3,4,5
+print(Tuple_List[0])
+
+##Set
+Sets = {"Mamadou", "Korka", "Mamadou"}
+print(Sets)
+print("Mamadou" in Sets)
